715098033010f1b2dd821fccceee55c9de1e2eab
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2022-01-31 21:51:57 +0300

    Implement portfolio and its entries

--- src/components/PortfolioEntry.vue
+++ src/components/PortfolioEntry.vue
@@ -1,30 +1,123 @@
 <template>
-  <div>
-    <div v-if="coinData" class="portfolio-entry">
-      <img :src="coinData.image.small" alt="" />
-      <span class="name">{{ coinData.localization.ru }}</span>
-      <span class="price">{{ coinData.market_data.current_price.usd }}</span>
-      <span class="price_change_percentage_24h">{{
-        coinData.market_data.price_change_percentage_24h
-      }}</span>
-      <span class="price_change_percentage_7d">{{
-        coinData.market_data.price_change_percentage_7d
-      }}</span>
-      <span class="market_cap">{{ coinData.market_data.market_cap.usd }}</span>
-      coinData.market_data.sparkline_7d
-    </div>
+  <tbody v-if="coinData" class="portfolio-entry">
+    <tr>
+      <td class="name">
+        <img :src="coinData.image.thumb" alt="" />
+        <span class="name">{{ coinData.name }}</span>
+      </td>
+      <td>
+        <div class="holdings">
+          <span>{{ holdingsCrypto }}</span>
+          <span>{{ holdingsUsd }}</span>
+        </div>
+      </td>
+      <td class="right">
+        <span class="price"
+          >{{ coinData.market_data.current_price.usd }} $</span
+        >
+      </td>
+      <td class="right">
+        <span class="price-change" :class="{ decreased: priceChange24h < 0 }"
+          >{{ priceChange24h }}%</span
+        >
+      </td>
+      <td class="right">
+        <span class="price-change" :class="{ decreased: priceChange7d < 0 }"
+          >{{ priceChange7d }}%</span
+        >
+      </td>
+      <td>
+        <span class="market_cap">{{ marketCap }} $</span>
+      </td>
+      <td>
+        <Chart
+          class="chart"
+          :chartDataProp="sparklineData"
+          :displayTooltip="false"
+          :displayAxes="false"
+          :displayGrid="false"
+          :fillSpaceUnderLine="false"
+        />
+      </td>
+    </tr>
+  </tbody>
 
-    <div v-else class="portfolio-entry-loader">
+  <tbody v-else class="portfolio-entry-loader">
+    <tr>
       <span>Загрузка...</span>
-    </div>
-  </div>
+    </tr>
+  </tbody>
 </template>
 
 <script>
+import Chart from "./Chart.vue";
 export default {
+  components: { Chart },
   props: ["currency", "coinData"],
+  computed: {
+    priceChange24h() {
+      return (
+        Math.round(this.coinData.market_data.price_change_percentage_24h * 10) /
+        10
+      );
+    },
+    priceChange7d() {
+      return (
+        Math.round(this.coinData.market_data.price_change_percentage_7d * 10) /
+        10
+      );
+    },
+    marketCap() {
+      return this.coinData.market_data.market_cap.usd.toLocaleString("ru-RU");
+    },
+    holdingsCrypto() {
+      return this.currency.value + " " + this.currency.label;
+    },
+    holdingsUsd() {
+      return (
+        this.currency.value * this.coinData.market_data.current_price.usd + " $"
+      );
+    },
+    sparklineData() {
+      const prices = this.coinData.market_data.sparkline_7d.price;
+      // let res = [];
+      let X = [];
+      for (let i = 0; i < prices.length; i++) {
+        X[i] = i + 1;
+      }
+
+      return {
+        X,
+        Y: prices,
+      };
+    },
+  },
 };
 </script>
 
 <style lang="sass" scoped>
+span.name
+  font-weight: 600
+  color: #0e1419
+  position: relative
+  top: 1px
+
+span.price-change
+  color: #4eaf0a
+span.decreased
+  color: #e15241
+
+.holdings
+  span
+    display: block
+
+.chart
+  height: 60px
+
+td.name *
+  vertical-align: middle
+
+img
+  width: 24px
+  padding: 0 13px 0 0
 </style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="portfolio">
     <span>Total balance usd:</span>
-    <div v-for="(currency, i) of portfolio" :key="i">
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <span class="name">Монета</span>
+          </th>
+          <th>
+            <span>Активы</span>
+          </th>
+          <th class="center">
+            <span>Цена</span>
+          </th>
+          <th class="center">
+            <span>24 ч</span>
+          </th>
+          <th class="center">
+            <span>7 д</span>
+          </th>
+          <th>
+            <span>Рыночная кап-ция</span>
+          </th>
+          <th><span>Последние 7 дней</span></th>
+        </tr>
+      </thead>
+
       <PortfolioEntry
+        v-for="(currency, i) of portfolio"
+        :key="i"
         :currency="currency"
         :coinData="portfolioData[currency.apiId]"
       />
-    </div>
+    </table>
   </div>
 </template>
 
@@ -27,5 +53,39 @@ export default {
 };
 </script>
 
+<style lang="sass">
+td, th
+  text-align: left
+  padding: 5px 12px
+  background: #fff
+  border-radius: 1px
+
+  &:first-child
+    border-top-left-radius: 6px
+    border-bottom-left-radius: 6px
+    border-collapse: collapse
+    border-right: none
+    border-spacing: 0
+  &:last-child
+    border-top-right-radius: 6px
+    border-bottom-right-radius: 6px
+
+.center
+  text-align: center
+.right
+  text-align: right
+</style>
+
 <style lang="sass" scoped>
+thead
+  span
+    color: #4a4a4a
+    font-weight: 600
+    font-size: .90em
+  th
+    padding: 10px 14px 9px 16px
+    text-align: center
+
+span.name
+  padding: 0 0 0 14px
 </style>
